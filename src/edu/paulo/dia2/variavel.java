298427e18cdd1bf0b8df86dbce3db38ff24a1f8f
Paulonts <paulo.henrique.nts@gmail.com>
2024-10-20 13:37:33 -0300

    atualização

--- src/edu/paulo/dia2/variavel.java
+++ src/edu/paulo/dia2/variavel.java
@@ -1,9 +1,21 @@
 package edu.paulo.dia2;
 
+/*
+byte idade = 123;
+short ano = 2021;
+int cep = 21070333
+long cpf = 95845742566L; tem que terminar com L para o java identificar que é um long
+float pi = 3.14F; tem que terminar com F para o java identificar que é um float
+double salario = 8546.55;
+*/
 public class variavel {
-    public static void main(String[] args) {
-        int calculadora = 10 + 10;
-        System.out.println(calculadora);
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500.33;
+        double desconto = 221.45;
+
+        double salario = salarioMinimo - desconto;
+
+        System.out.println("seu salario deu " + salario);
     }
-    
+
 }
---
+++ src/edu/paulo/dia2/variavel_constante.java
@@ -0,0 +1,17 @@
+package edu.paulo.dia2;
+
+public class variavel_constante {
+    public static void main(String[] args) throws Exception {
+        
+        //Variavel
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+        //constante
+        final float pi = 3.14F;
+        System.out.println(pi);
+
+    }
+    
+}
