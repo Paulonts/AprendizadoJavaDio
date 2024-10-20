e8b01637d23644633c8a48e5a636ba181616a283
Paulonts <paulo.henrique.nts@gmail.com>
2024-10-20 13:03:19 -0300

    atualização

--- src/edu/paulo/dia1/BoletimEstudantil.java
+++ src/edu/paulo/dia1/BoletimEstudantil.java
@@ -3,7 +3,7 @@ public class BoletimEstudantil {
 
     public static void main(String[] args) {
 
-        int mediaFinal = 6;
+        int mediaFinal = 7;
 
         if (mediaFinal < 6) {
             System.out.println("Reprovado");
