b84ab62f0765fd24b0f2cce51c49f0febee746c0
Paulonts <paulo.henrique.nts@gmail.com>
2024-10-20 13:11:53 -0300

    atualizção

--- src/edu/paulo/dia2/variavel.java
+++ src/edu/paulo/dia2/variavel.java
@@ -1,5 +1,9 @@
 package edu.paulo.dia2;
 
 public class variavel {
+    public static void main(String[] args) {
+        int calculadora = 10 + 5;
+        System.out.println(calculadora);
+    }
     
 }
